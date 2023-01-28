827716d2d35d5a2ed35fde9350e6782fba0a5bda
Abdullah Khan <92824206+abdullahkhn77@users.noreply.github.com>
2023-01-28 22:21:17 +0500

    Add files via upload

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+
+import { urlFor } from '../lib/client';
+
+const HeroBanner = ({ heroBanner }) => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="banner">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+
+      
+        </div>
+      </div>
+  )
+}
+
+export default HeroBanner
